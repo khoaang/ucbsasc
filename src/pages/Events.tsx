d9832440f0d79f1b836a6380aa453bb631b184a2
khoaang <khoan@berkeley.edu>
2025-02-04 21:30:46 -0800

    Added night market to events page

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -1,18 +1,21 @@
 import { Container, Typography } from '@mui/material';
 import Footer from '../components/Footer';
+import NightMarket from '../components/events/NightMarket';
 
 const Events = () => {
   return (
     <>
-    <Container sx={{ py: 8 }}>
-      <Typography variant="h2" component="h1" gutterBottom>
-        Events
-      </Typography>
-      <Typography variant="body1">
-        Our upcoming events will be listed here.
-      </Typography>
-    </Container>
-    <Footer />
+      <Container sx={{ py: 8 }}>
+        <Typography variant="h2" component="h1" gutterBottom>
+          Events
+        </Typography>
+        <NightMarket />
+        {/* <CultureFestival /> */}
+        <Typography variant="body1" sx={{ mt: 4 }}>
+          More upcoming events will be listed here.
+        </Typography>
+      </Container>
+      <Footer />
     </>
   );
 };
